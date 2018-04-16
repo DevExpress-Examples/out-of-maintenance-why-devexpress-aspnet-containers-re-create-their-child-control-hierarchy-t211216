52442c70e7bef3346cc37e1f44de28d89fb99e6b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-16 12:42:10 +0300

    initial commit

---
+++ CS/Default.aspx.cs
@@ -0,0 +1,16 @@
+﻿using DevExpress.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+public partial class _Default : System.Web.UI.Page
+{
+	protected void Page_Load(object sender, EventArgs e) {
+		if (!IsPostBack) {
+			rp1.View = View.GroupBox;
+			rp2.View = View.GroupBox;
+		}
+	}
+}
---
+++ VB/Default.aspx.vb
@@ -0,0 +1,17 @@
+﻿Imports DevExpress.Web
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+
+Partial Public Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+        If Not IsPostBack Then
+            rp1.View = View.GroupBox
+            rp2.View = View.GroupBox
+        End If
+    End Sub
+End Class
